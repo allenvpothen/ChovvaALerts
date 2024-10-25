ba236a01248b314900d8b6f833862e8c570427d8
jayasurya jayakumar <107751867+J4Y45URY4@users.noreply.github.com>
2024-10-25 20:30:38 +0000

    Changes to be committed:

--- login/login.js
+++ login/login.js
@@ -1,5 +1,6 @@
 const nameInput = document.querySelector(".name")
 const nameOutput = document.querySelector(".nameDupe")
+const submitButton = document.querySelector(".submit")
 nameInput.addEventListener("input", nameReverser);
 function nameReverser() {
     let realNameArray = nameInput.value.split("")
@@ -7,3 +8,23 @@ function nameReverser() {
     let reversedName = reversedNameArray.join("")
     nameOutput.value = reversedName;
 }
+const form = document.getElementById("userForm");
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    
+    const name = nameInput.value;
+    const age = ageInput.value;
+
+   
+    localStorage.setItem("name", name);
+    localStorage.setItem("age", age);
+
+   
+    window.location.href = "../main/main.html"; // Change "nextPage.html" to your desired page
+});
+submitButton.addEventListener("mouseover", () => {
+    const randomX = Math.random() * 5; 
+    const randomY = Math.random() * 3; 
+    submitButton.style.transform = "translate("+ randomX+ "rem,"+randomY+"rem)";
+});
