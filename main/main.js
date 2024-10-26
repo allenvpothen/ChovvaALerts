8da14ab9d1678e41e3daac14bf7642ecc169559d
allenvpothen <allenvpothen2005@gmail.com>
2024-10-26 06:34:52 +0530

    modified:   main/main.html 	modified:   main/main.js

--- main/main.js
+++ main/main.js
@@ -23,6 +23,9 @@ const textureLoader = new THREE.TextureLoader();
 const marsBaseColor = textureLoader.load("textures/mars_color.jpg");
 const marsNormal = textureLoader.load("textures/mars_normal.jpg");
 const pinTexture = textureLoader.load("textures/pin.png");
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
 
 //camera
 camera.position.z = 35;
@@ -39,6 +42,7 @@ const directionalLight = new THREE.DirectionalLight(0xdec9a4, 2);
 directionalLight.position.set(-1, 0.3, 0.7);
 scene.add(directionalLight);
 // camera.add(directionalLight);
+
 scene.add(camera);
 
 //mars
@@ -50,6 +54,12 @@ const marsMaterial = new THREE.MeshStandardMaterial({
 
 const mars = new THREE.Mesh(marsGeometry, marsMaterial);
 scene.add(mars);
+
+
+let INTERSECTED = mars;
+// camera.position.set(INTERSECTED.position)
+
+camera.zoom = 1;
 
 //locators
 const locatorGeometry = new THREE.PlaneGeometry(1, 1);
@@ -63,72 +73,90 @@ const northPole = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(northPole);
 northPole.position.set(0, 18, 0);
 northPole.scale.set(1.5, 1.5, 1.5);
+northPole.name = "North Pole";
 
 const southPole = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(southPole);
 southPole.position.set(0, -18, 0);
 southPole.scale.set(1.5, 1.5, 1.5);
+southPole.name = "South Pole";
+
 
 const galeCrater = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(galeCrater);
 galeCrater.position.set(-17, -3, 0);
 galeCrater.scale.set(1.5, 1.5, 1.5);
+galeCrater.name = "Gale Crater";
+
 
 const jezeroCrater = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(jezeroCrater);
 jezeroCrater.position.set(17, 3, 0);
 jezeroCrater.scale.set(1.5, 1.5, 1.5);
+jezeroCrater.name = "Jezero Crater";
+
 
 const olympusMons = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(olympusMons);
 olympusMons.position.set(9, -3, 14);
 olympusMons.scale.set(1.5, 1.5, 1.5);
+olympusMons.name = "Olympus Mons";
+
+
 
 const miyamotoCrater = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(miyamotoCrater);
 miyamotoCrater.position.set(-2, -1, 17);
 miyamotoCrater.scale.set(1.5, 1.5, 1.5);
+miyamotoCrater.name = "Miyamoto Crater";
+
 
 const eddieCrater = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(eddieCrater);
 eddieCrater.position.set(2, -2, -17);
 eddieCrater.scale.set(1.5, 1.5, 1.5);
+eddieCrater.name = "Eddie Crater";
+
 
 const keplerCrater = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(keplerCrater);
 keplerCrater.position.set(-4, -5, -14);
 keplerCrater.scale.set(1.5, 1.5, 1.5);
+keplerCrater.name = "Kepler Crater";
+
 
 const mawrthVallis = new THREE.Mesh(locatorGeometry, locatorMaterial);
 mars.add(mawrthVallis);
 mawrthVallis.position.set(-15, 8, -2);
 mawrthVallis.scale.set(1.5, 1.5, 1.5);
+mawrthVallis.name = "Mawarth Vallis";
 
-// locator1.rotateZ(Math.PI/2);
 
-//Locator 2.0
-// let allParticles = [
-//     { name: 'a', position: new THREE.Vector3( 400, 300, 300 ) },
-//     { name: 'b', position: new THREE.Vector3( 300, 400, 300 ) },
-//     { name: 'c', position: new THREE.Vector3( 100, 100, -100 ) },
-//     { name: 'd', position: new THREE.Vector3( -300, -200, -300 ) },
-//     { name: 'e', position: new THREE.Vector3( -400, 400, -200 ) },
-//     { name: 'f', position: new THREE.Vector3( 500, -300, -500 ) },
-// ];
-// const locatorSphereGeometry = new THREE.SphereGeometry(16.5, 6, 3);
-// locatorSphereGeometry.deleteAttribute( 'uv' );
-// const locatorSphereMaterial = new THREE.PointsMaterial({
-//     map: pinTexture,
-//     color: "rgb(210, 43, 43)",
-//     transparent: true,
-//     size: 2,
-//     alphaMap: pinTexture,
-// });
-// const particleSystem = new THREE.Points(locatorSphereGeometry, locatorSphereMaterial);
-// particleSystem.name = "particleSystem";
-// particleSystem.rotation.z = 1;
-// particleSystem.rotation.y = 1.2;
-// scene.add(particleSystem)
+//Clicks
+let site = INTERSECTED.name;
+
+document.addEventListener("click", clicked);
+function clicked(event) {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(pointer, camera);
+  const intersects = raycaster.intersectObjects(mars.children, false);
+
+  if (intersects.length > 0) {
+    if (INTERSECTED != intersects[0].object) {
+      if (INTERSECTED)
+    // camera.autoRotate
+        // document.querySelector("canvas").style.left = "-20rem"
+        // camera.position.x = 15;
+        
+      INTERSECTED = intersects[0].object;
+    }
+  } else {
+    if (INTERSECTED)
+    INTERSECTED = INTERSECTED;
+  }
+}
 
 //Update
 window.addEventListener("resize", () => {
@@ -154,8 +182,9 @@ function animate() {
   eddieCrater.lookAt(camera.position);
   keplerCrater.lookAt(camera.position);
   mawrthVallis.lookAt(camera.position);
-
-
+//   camera.lookAt(INTERSECTED.position)
+// camera.position.set(INTERSECTED.position)
+camera.updateProjectionMatrix()
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
